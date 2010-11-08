6ff079013116d5280bcfca4f64455728e259cbf1
EnTrERy <EnTrERy@EnTrERy-PC>
2010-11-08 19:23:37 +0200

    .

---
+++ src/main/java/jia/exercises/lecture2/Aleksander/Zlatkovski/Exercise1.java
@@ -0,0 +1,33 @@
+package jia.exercises.lecture2.Aleksander.Zlatkovski;
+
+
+import java.util.*;
+import java.io.*;
+public class Exercise1 {
+	
+	public static void main ( String [] args ) throws IOException {
+	
+	
+	   Scanner sc = new Scanner(System.in);
+	   String name="",lastName="",post="",age="",salary="";
+	   PrintWriter pw = new PrintWriter((new FileWriter(new File("Sample.txt"), true)),true);
+       
+       //FileWriter fstream = new FileWriter("Sample.txt",true);
+	   //BufferedWriter out = new BufferedWriter(fstream);
+	    
+	   System.out.println("name:");
+	   name=sc.nextLine();
+	   System.out.println("last name:");
+	   lastName=sc.nextLine();
+	   System.out.println("age:");
+	   age=sc.nextLine();
+	   System.out.println("post:");
+	   post=sc.nextLine();
+	   System.out.println("salary:");
+	   salary=sc.nextLine();
+	   
+	   pw.println(String.format ("%10s",name)+String.format ("%12s",lastName)+"   "+String.format ("%3s",age)+String.format ("%12s",post)+" "+String.format ("%8s",salary));
+	   pw.close();
+		
+	   }
+	}
